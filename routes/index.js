a745dfbde6f45065cb74afef60c57162fa647690
Rushabh2499 <rushabh171105066@gmail.com>
2020-09-03 03:49:45 +0530

    Parameterized links implemented for destinations

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,14 @@ var router = express.Router();
 const csv = require('csv-parser');
 const fs = require('fs');
 
+function capital_letter(str){
+    str = str.split(" ");
+    for (var i = 0, x = str.length; i < x; i++){
+        str[i] = str[i][0].toUpperCase() + str[i].substr(1);
+    }
+    return str.join(" ");
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'ejs' });
@@ -13,36 +21,35 @@ router.get('/about', function (req, res) {
   res.render('about', { title: 'ejs' })
 });
 
-router.get('/destination', function (req, res) {
+router.get('/destinput', function (req, res) {
   res.render('destinput', { title: 'ejs' })
 });
 
-router.post('/destination', function(req, res) {
-	var input = req.body.ipcity.toLowerCase();
-	console.log(input);
+router.get('/destination/:id', function(req, res) {
+	//code for search
+	/*var input = req.body.ipcity.toLowerCase();*/ 
+	var input = capital_letter(req.params.id);
 	var spot;
- fs.createReadStream('data.csv')
-  .pipe(csv())
-  .on('data', (row) => {
-    spot = row['TouristSpot'];
-    if(spot == input){
-    	var city = row['CityName'];
-    var dscrp = row['Description'];
-    var img = row ['Images'].split(',');
-    for (i in img){
-    	img[i] = img[i].substr(2, img[i].length-3);
-    	console.log(img[i]);
-    }
-    img[img.length-1] = img[img.length-1].substr(0, img[i].length-1);
-    console.log(spot);
-    res.render('destination', { city: city, spot: spot, description: dscrp , img: img})
-    }
-  })
-  .on('end', () => {
-    console.log('CSV file successfully processed');
-    res.render('error', { title: 'ejs' });
-  })
+	fs.createReadStream('data.csv')
+	  .pipe(csv())
+	  .on('data', (row) => {
+	    spot = row['TouristSpot'];
+	    if(spot == input){
+	    	var city = row['CityName'];
+	    var dscrp = row['Description'];
+	    var img = row ['Images'].split(',');
+	    for (i in img){
+	    	img[i] = img[i].substr(2, img[i].length-3);
+	    }
+	    img[img.length-1] = img[img.length-1].substr(0, img[i].length-1);
+	    console.log(spot);
+	    res.render('destination', { city: city, spot: spot, description: dscrp , img: img})
+	    return;
+	    }
+	  })
+  
 });
 
 
 module.exports = router;
+
